af4cb3a39b582f9ed779f18a8b6f31ec832c7d13
Sebastian Tamayo <sebastian-tamayo.pacheco@brightstraining.com>
2023-05-27 22:19:04 +0200

    205. Isomorphic Strings : Javascript.

--- Day2/IsomorphicStrings/Javascript/205.js
+++ Day2/IsomorphicStrings/Javascript/205.js
@@ -3,8 +3,45 @@
  * @param {string} t
  * @return {boolean}
  */
-var isIsomorphic = function(s, t) {
+var isIsomorphic = function (s, t) {
 
+    let isSizeCorrect = (s.length >= 1 && s.length <= (5 * 100000));
+    let isStringLengthEqual = s.length === t.length;
 
+    if (!isSizeCorrect || !isStringLengthEqual) {
+        return false;
+    }
 
+    let charMapSToT = {};
+    let charMapTToS = {};
+
+    for (let i = 0; i < s.length; i++) {
+        let sChar = s.charAt(i);
+        let tChar = t.charAt(i);
+
+        let isSMapFalselyMapped = (
+            charMapSToT.hasOwnProperty(sChar) &&
+            (charMapSToT[sChar] !== tChar)
+        );
+
+        let isTMapFalselyMapped = (
+            charMapTToS.hasOwnProperty(tChar) &&
+            (charMapTToS[tChar] !== sChar)
+        );
+
+        if (isSMapFalselyMapped) {
+            return false;
+        }
+
+        if (isTMapFalselyMapped) {
+            return false;
+        }
+
+        charMapSToT[sChar] = tChar;
+        charMapTToS[tChar] = sChar;
+
+    }
+    console.log(charMapSToT)
+    console.log(charMapTToS)
+    return true;
 };
